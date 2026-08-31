2f25f4752b4e632c1f64aa55dc8e01bbb7445607
VaibhavDW789 <vaibhavw97531@gmail.com>
2026-08-31 13:29:43 +0530

    Create secure S3 state bucket

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -2,3 +2,13 @@ output "message" {
   description = "Bootstrap configuration status"
   value       = "Terraform remote state bootstrap configuration is ready"
 }
+
+output "state_bucket_name" {
+  description = "Name of the Terraform state bucket"
+  value       = aws_s3_bucket.terraform_state.bucket
+}
+
+output "state_bucket_arn" {
+  description = "ARN of the Terraform state bucket"
+  value       = aws_s3_bucket.terraform_state.arn
+}
--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "AWS region where the Terraform state bucket will be created"
   type        = string
 }
+
+variable "state_bucket_name" {
+  description = "Globally unique name for the Terraform state bucket"
+  type        = string
+}
